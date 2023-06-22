f9a77ae198d8fb2e04f978b5ceb7bc46482c3761
pablomoty <pablomoty@gmail.com>
2023-06-21 23:12:05 -0300

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainWrapperComponent   
   ],
   imports: [
     BrowserModule
